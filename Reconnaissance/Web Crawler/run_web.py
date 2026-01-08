c92a54ed02313364be01b411404c0767446c90e2
Jackson Kosmer <jackson.kosmer@proton.me>
2026-01-08 22:59:52 +1100

    Moved web crawler to 'Vigilare' repo

--- Reconnaissance/Web Crawler/run_web.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-
-if __name__ == "__main__":
-    print("--- Vigilo Search Interface Running ---")
-    print("Open http://127.0.0.1:5000 in your browser.")
-    app.run(debug=True, host="0.0.0.0", port=5000)
